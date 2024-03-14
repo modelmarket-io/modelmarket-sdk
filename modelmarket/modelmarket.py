d0cbe35baacfacb9a97e06c47ab472b865d56cdc
Adrian Gortzak <addegor95@gmail.com>
2024-03-14 10:20:42 +0100

    Update modelmarket.py
    Fixes to align with v.0.2

--- modelmarket/modelmarket.py
+++ modelmarket/modelmarket.py
@@ -16,13 +16,13 @@ class Client:
         if self.debug:
             print("Auth url: ", url)
 
-        payload = {
+        payload = json.dumps({
             "username": username,
             "password": password
-        }
+        })
 
         headers = {
-            'Content-Type': 'application/x-www-form-urlencoded'
+            'Content-Type': 'application/json'
         }
 
         response = requests.post(url, headers=headers, data=payload)
@@ -44,44 +44,32 @@ class Client:
         self.access_token = json_response['access_token']
         self.refresh_token = json_response['refresh_token']
 
-    def models(self, df, provider="", model_name="", model_type="normal", chunk_size=10000):
+    def models(self, input_features={}, provider="", model_name="", model_type="normal"):
         url = self.server_url + "/v1/models/" + model_type + "/" + provider + "/" + model_name
         # print(url)
-        predict_column = provider + "-" + model_name
-        full_predictions_df = pd.DataFrame()
-
-        # Iterate through DataFrame chunks
-        for i in range(0, len(df), chunk_size):
-            chunk_df = df.iloc[i:i + chunk_size]
-
-            # Extract the 'data' field and convert it to a Dict to match your desired format
-            payload_dict = self.df_api_input(df)
-            # print(self.access_token)
-            payload = json.dumps(payload_dict)
-            headers = {
+        headers = {
                 'accept': 'application/json',
                 'Content-Type': 'application/json',
                 'Authorization': 'Bearer ' + self.access_token
             }
-
-            # Make the request
-            response = requests.post(url, headers=headers, data=payload)
-
-            # Check if the request was successful
-            if response.status_code != 200:
+        
+        
+        
+            payload = json.dumps(input_features)
+        
+            response = requests.request("POST", url, headers=headers, data=payload)
+        
+            if response.status_code == 401:
                 raise Exception(
-                    f"You do not have the necessary permissions to access this model. Please check your access rights or contact the administrator for assistance. Error:[{response.status_code}]")
-
-                # Extract the predictions
-            predictions = response.json()['predictions']
-            # print(predictions)
-            predictions_df = pd.DataFrame(list(predictions.items()), columns=['row_nr',
-                                                                              predict_column])
-
-            # Concatenate the prediction chunks
-            full_predictions_df = pd.concat([full_predictions_df, predictions_df], ignore_index=True)
-
-        return full_predictions_df[predict_column]
+                    "Unauthorized access detected. You do not have the necessary permissions to access this model. Please verify your credentials and try again. Error: [401 Unauthorized]")
+            elif response.status_code == 500:
+                raise Exception(
+                    "An internal server error occurred. This might be due to issues within the server or the model itself. Please try again later or contact the administrator for assistance. Error: [500 Internal Server Error]")
+            elif response.status_code != 200:
+                raise Exception(
+                    f"An unexpected error occurred. Please check your request and try again, or contact the administrator for assistance. Error: [{response.status_code}]")
+        
+            return response.json()
 
     def df_api_input(self, df):
         payload = df.to_json(orient="split")
